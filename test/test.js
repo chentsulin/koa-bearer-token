48d7d50d922966433c23038900312704ce4c06fd
C.T. Lin <chentsulin@gmail.com>
2015-04-27 01:52:52 +0800

    add test

--- test/test.js
+++ test/test.js
@@ -1,7 +1,10 @@
 var bearerToken = require('../')
 var request = require('supertest')
+var expect = require('chai').expect
 var koa = require('koa')
 var app
+
+var token = '1234567890abcdefghijk'
 
 
 describe('koa-bearer-token', function() {
@@ -9,16 +12,60 @@ describe('koa-bearer-token', function() {
     app = koa()
 
     app.use(bearerToken())
-  })
-
-  it('token can place in header', function(done) {
 
     app.use(function *() {
-      // this.request.token
+      console.log('this.request.token', this.request.token)
+      if (this.request.token) {
+        this.body = this.request.token
+      } else {
+        this.body = 'undefined'
+      }
     })
+  })
+
+  it('token should be undefined when no token provided', function(done) {
 
     request(app.listen())
     .get('/')
+    .expect(function(res) {
+      if (res.text !== 'undefined') return 'res text !== undefined'
+    })
+    .end(done)
+
+  })
+
+  it('token can be provided in header', function(done) {
+
+    request(app.listen())
+    .get('/')
+    .set('Authorization', 'Bearer ' + token)
+    .expect(function(res) {
+      if (res.text !== token) return 'res text !== token'
+    })
+    .end(done)
+
+  })
+
+  it('token can be provided in query', function(done) {
+
+    request(app.listen())
+    .get('/')
+    .query( { access_token: token })
+    .expect(function(res) {
+      if (res.text !== token) return 'res body !== token'
+    })
+    .end(done)
+
+  })
+
+  it('token can be provided in body', function(done) {
+
+    request(app.listen())
+    .post('/')
+    .send({ access_token: token })
+    .expect(function(res) {
+      if (res.text !== token) return 'res text !== token'
+    })
     .end(done)
 
   })
